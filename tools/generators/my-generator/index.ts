35327cffca978965fc6d1fc561a3bf0878569039
Christian Liebel <christian.liebel@thinktecture.com>
2022-10-18 10:09:39 +0200

    Lab #4

--- tools/generators/my-generator/index.ts
+++ tools/generators/my-generator/index.ts
@@ -1,8 +1,15 @@
 import {Tree} from '@nrwl/devkit';
+import {applicationGenerator} from "@nrwl/angular/generators";
 
 interface Schema {
   name: string;
 }
 
 export default async function (tree: Tree, schema: Schema) {
+  await applicationGenerator(tree, {
+    name: schema.name,
+    routing: true,
+    prefix: schema.name,
+    style: 'scss',
+  });
 }
